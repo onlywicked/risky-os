3fec7d42ee33ed1e334d5066c56a0fa69209f414
Arman <hello@arman.work>
2025-01-27 04:25:52 +0530

    chore: say hello world

---
+++ common.c
@@ -0,0 +1,76 @@
+#include "common.h"
+
+void putchar(char ch);
+
+void printf(const char *fmt, ...) {
+  va_list vargs;
+  va_start(vargs, fmt);
+
+  while (*fmt) {
+    if (*fmt == '%') {
+      fmt++; // Skip '%'
+
+      switch (*fmt) { // Read the next character
+      case '\0':      // '%' at the end of the format string
+        putchar('%');
+        goto end;
+
+      case '%':
+        putchar('%');
+        break;
+
+      case 's': { // Print a NULL-terminated string.
+        const char *s = va_arg(vargs, const char *);
+        while (*s) {
+          putchar(*s);
+          s++;
+        }
+        break;
+      }
+
+      case 'd': { // Print an integer in decimal
+        int value = va_arg(vargs, int);
+
+        if (value < 0) {
+          putchar('-');
+          value = -value;
+        }
+
+        int divisor = 1;
+
+        while (value / divisor > 9) {
+          divisor *= 10;
+        }
+
+        while (divisor > 0) {
+          putchar('0' + value / divisor);
+          value %= divisor;
+          divisor /= 10;
+        }
+
+        break;
+      }
+      case 'x': { // Print an integer in hexadecimal
+        int value = va_arg(vargs, int);
+
+        putchar('0');
+        putchar('x');
+
+        for (int i = 7; i >= 0; i--) {
+          int nibble = (value >> (i * 4)) & 0xf;
+
+          putchar("0123456789abcdef"[nibble]);
+        }
+      }
+        // switch end
+      }
+    } else {
+      putchar(*fmt);
+    }
+
+    fmt++;
+  }
+
+end:
+  va_end(vargs);
+}
---
+++ kernel.c
@@ -0,0 +1,69 @@
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+typedef uint32_t size_t;
+
+#include "kernel.h"
+#include "common.h"
+
+extern char __bss[], __bss_end[], __stack_top[];
+
+struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
+                       long arg5, long fid, long eid) {
+
+  register long a0 __asm__("a0") = arg0; // function arguments/return values
+  register long a1 __asm__("a1") = arg1; // function arguments/return values
+
+  register long a2 __asm__("a2") = arg2; // function arguments
+  register long a3 __asm__("a3") = arg3; // function arguments
+  register long a4 __asm__("a4") = arg4; // function arguments
+  register long a5 __asm__("a5") = arg5; // function arguments
+  register long a6 __asm__("a6") = fid;  // function arguments
+  register long a7 __asm__("a7") = eid;  // function arguments
+
+  __asm__ __volatile__("ecall"
+                       : "=r"(a0), "=r"(a1)
+                       : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
+                         "r"(a6), "r"(a7)
+                       : "memory");
+
+  return (struct sbiret){.error = a0, .value = a1};
+}
+
+void putchar(char ch) {
+  sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
+}
+
+// memset initializes a buffer of size n with the given value.
+void *memset(void *buf, char c, size_t n) {
+  uint8_t *p = (uint8_t *)buf;
+
+  while (n--) {
+    *p++ = c;
+  }
+
+  return buf;
+}
+
+void kernel_main(void) {
+  printf("\n\nHello World!\n");
+
+  printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+
+  for (;;) {
+    __asm__ __volatile__("wfi");
+  }
+}
+
+/**
+ * The execution of the kernel starts from 'boot' function, which is specified
+ * as the entry point in our linker file.
+ **/
+__attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void) {
+  __asm__ __volatile__(
+      "mv sp, %[stack_top]\n" // set the stack pointer
+      "j kernel_main\n"       // jump to the kernel main function
+      :
+      : [stack_top] "r"(
+          __stack_top) // pass the stack top address as %[stack_top]
+  );
+}
